948eacb8f773f50522ea5daecddfe613008df231
tort134 <knatashambler.18@gmail.com>
2025-02-04 13:55:57 +0700

    complete 6 pract task

--- js/main.js
+++ js/main.js
@@ -9,20 +9,45 @@ let app = new Vue({
             this.cart -= 1
         },
 
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         },
     },
 
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+
+        sale(){
+            if(this.onSale){
+                return this.brand + ' ' + this.product + ' ' + "Sale now!";
+            }
+
+                return this.brand + ' ' + this.product + ' ' + "Not sale :(";
+        },
+
+    },
+
+
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        selectedVariant: 0,
+        onSale: true,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
-        onSale: true,
-        inventory: 0,
+        inventory: 10,
 
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
 
@@ -31,13 +56,16 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
+
 
 
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
