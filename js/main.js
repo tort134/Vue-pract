30cbe88a15595a869ecab21de34a06e2d92ea9e4
tort134 <knatashambler.18@gmail.com>
2025-02-04 11:08:51 +0700

    complete 4 pract task

--- js/main.js
+++ js/main.js
@@ -9,6 +9,24 @@ let app = new Vue({
         inStock: true,
         onSale: true,
         inventory: 100,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+
+
+
+
 
     }
 })
