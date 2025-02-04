4317cc6ade4245a5f4543cf054b067d29dcccde2
tort134 <knatashambler.18@gmail.com>
2025-02-04 10:43:11 +0700

    complete 3 pract task

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ let app = new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true
+        inStock: true,
+        onSale: true,
+        inventory: 100,
 
     }
 })
